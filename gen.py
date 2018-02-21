60eb4030d7134b4311daa3b98ad622861b855363
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-21 12:07:51 +0000

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,9 +1,12 @@
 import random
 
+min = 1
+max = raw_input("How many sides does your dice have? ")
 how_many_dice = raw_input("How many dice are you playing with? ")
+
 values = []
 
 for x in range(0, int(how_many_dice)):
-  values.append(random.randint(1,6))
+  values.append(random.randint(min, int(max)))
   
 print values
