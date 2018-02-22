d62a3292abaf5835a8f8344769b610149abdedb8
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-22 13:25:39 +0000

    Update gen.py
    Fixes #1

--- gen.py
+++ gen.py
@@ -1,13 +1,23 @@
 import random
 
+play_again = True
+
 min = 1
-max = raw_input("How many sides does your dice have? ")
-how_many_dice = raw_input("How many dice are you playing with? ")
 
-values = []
+while play_again:
+	max = raw_input("How many sides does your dice have? ")
+	how_many_dice = raw_input("How many dice are you playing with? ")
 
-for x in range(0, int(how_many_dice)):
-  values.append(random.randint(min, int(max)))
+	values = []
+
+	for x in range(0, int(how_many_dice)):
+		values.append(random.randint(min, int(max)))
   
-print
-print values
+	print
+	print values
+	print
+
+	again = raw_input("Do you want to roll again? ")
+	print
+	if again.lower() == "no" or again.lower() == "n":
+		play_again = False
