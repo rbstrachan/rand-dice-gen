daf6e8eada303a1a996cbeb2cef671f1dc029ed9
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-23 04:23:05 +0000

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,27 +1,37 @@
 import random
 
 play_again = True
-
+total = 0
 min = 1
 
+how_many_dice = input("How many dice are you playing with? ")
+max = input("How many sides does your dice have? ")
+
 while play_again:
-	max = raw_input("How many sides does your dice have? ")
-	how_many_dice = raw_input("How many dice are you playing with? ")
+  print()
+  values = []
 
-	values = []
+  for y in range(0, int(how_many_dice)):
+    values.append(random.randint(min, int(max)))
+    print("Dice #" + str(y + 1) + ": " + str(values[y]))
 
-	for x in range(0, int(how_many_dice)):
-		values.append(random.randint(min, int(max)))
-  
-	print
-  
-	for x in values:
-		print x
+  print()
+  for x in range(0, len(values)):
+    total += values[x]
+  print("Total: " + str(total))
+  print()
 
-	print
-
-	again = raw_input("Do you want to roll again? ")
-	if again.lower() == "no" or again.lower() == "n":
-		play_again = False
-	else:
-		print
+  choice = input("Do you want to [roll] again, [change] your dice or [stop] rolling? ").lower()
+  if choice == "stop rolling" or choice == "stop":
+    play_again = False
+  elif choice == "change dice" or choice == "change":
+    total = 0
+    how_many_dice = input("How many dice are you now playing with? ")
+    max = input("And those dice have how many sides? ")
+    print("Rolling...")
+  elif choice == "roll again" or choice == "roll":
+    total = 0
+    print("Rolling...")
+  else:
+    print("Sorry, that's not a valid response. Quitting program.")
+    play_again = False
